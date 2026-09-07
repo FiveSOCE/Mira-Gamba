906ccf62e3b29955d86f511141e71f77916fe204
FiveS <sgmmsionide@gmail.com>
2026-09-08 03:22:41 +1000

    Bootstrap MiraGamba plugin

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+}
+
+group = "com.mira"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(21)
+}
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.jar {
+    archiveFileName.set("MiraGamba-${project.version}.jar")
+}
